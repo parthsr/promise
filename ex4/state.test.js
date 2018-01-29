b7851d177ad2f39efaae497af49a7777bb2e8ac7
parth <srivastavaparth2016@gmail.com>
2018-01-30 01:02:33 +0530

    adding test 2 for state.js

--- ex4/state.test.js
+++ ex4/state.test.js
@@ -4,4 +4,7 @@ describe('checking use case', () => {
   it('test case 1', () => {
     expect(state).resolves.toBe('I FIRED');
   });
+  it('test case 1', () => {
+    expect(state).rejects.toMatch('I DID NOT FIRED');
+  });
 });
