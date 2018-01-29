98031a17fddcbb4ef912f772ac546697b564fb25
parth <srivastavaparth2016@gmail.com>
2018-01-29 12:17:11 +0530

    setTimeout.js

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules":{
+      "no-console": 0,
+      "no-var": "error",
+      "no-undef": 0,
+      "indent": [
+          "error", 2, { "FunctionDeclaration": {"body": 1, "parameters": 2} }
+      ],
+      "prefer-rest-params": 0
+  }
+};
