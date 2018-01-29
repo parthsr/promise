3949c8f841eaeb02353eb8045ae5b1cd25849a23
parth <srivastavaparth2016@gmail.com>
2018-01-29 23:53:57 +0530

    test case 4 for reject.js

--- ex3/reject.test.js
+++ ex3/reject.test.js
@@ -12,7 +12,9 @@ describe('describing the use case of the program', () => {
     reject.onReject('hello');
     expect((console.log)).toHaveBeenCalledTimes(1);
   });
-  // it('testCase 2 for checking execution', () => {
-  //   expect(reject.promise).resolves.toBe('pass');
-  // });
+  it('testCase 3 for checking onReject', () => {
+    (global.console.log) = jest.fn();
+    reject.onReject(new Error('kk'));
+    expect((console.log)).toHaveBeenCalledTimes(1);
+  });
 });
