3b89a7343b76d0d426ba018a65e81b5191c75896
parth <srivastavaparth2016@gmail.com>
2018-01-30 12:03:24 +0530

    test case refractoring

--- ex3/reject.test.js
+++ ex3/reject.test.js
@@ -1,12 +1,7 @@
 const reject = require('./reject');
 
 describe('describing the use case of the program', () => {
-  it('test case 1 for checking error', () => {
-    expect(reject.promise).rejects.toMatch('REJECTED!');
-  });
-  it('testCase 2 for checking execution', () => {
-    expect(reject.promise).resolves.toBe('pass');
-  });
+  it('test case 1 for checking error', () => expect(reject.promise).rejects.toEqual(Error('REJECTED!')));
   it('testCase 3 for checking onReject when fullfil', () => {
     (global.console.log) = jest.fn();
     reject.onReject('hello');
