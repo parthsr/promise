5ab91e0597075b98614f9b1bdcfab8a0f142ae54
parth <srivastavaparth2016@gmail.com>
2018-01-31 09:56:36 +0530

    adding test case for chain.js

--- ex8/chain.js
+++ ex8/chain.js
@@ -4,4 +4,21 @@ promise = new Promise((resolves) => {
   resolves('MANHATTAN');
 });
 
-promise.then(attachTitle).then(console.log);
+// console.log(promise.then(attachTitle));
+module.exports = {
+  promise,
+  attachTitle,
+};
+
+// const attachTitle = name => `DR. ${name}`;
+// const promise = new Promise((fulfill) => {
+//   fulfill('MANHATTAN');
+// });
+// const promiseCallback = (value) => {
+//   console.log('Yo', value);
+//   attachTitle(value);
+//   return 10;
+// };
+// const pp = promise.then(promiseCallback).then((value2) => {
+//   console.log('value2', value2);
+// });
--- ex8/chain.test.js
+++ ex8/chain.test.js
@@ -1,1 +1,7 @@
 const chain = require('./chain');
+
+describe('checking use case', () => {
+  it('checking if the printing is as expected', () => {
+    expect(chain.promise.then(chain.attachTitle).then(value => expect(value).toBe('DR. MANHATTAN')));
+  });
+});
