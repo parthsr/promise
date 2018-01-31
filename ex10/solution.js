afd851c736a386055b99a1d518fe78c74fbf50ec
parth <srivastavaparth2016@gmail.com>
2018-01-31 12:02:11 +0530

    adding test case for iterate funtion

--- ex10/solution.js
+++ ex10/solution.js
@@ -1,5 +1,9 @@
 const alwaysThrows = () => new Error('OH NOES');
-
+const iterate = (arg) => {
+  console.log(arg);
+  return arg + 1;
+};
 module.exports = {
   alwaysThrows,
+  iterate,
 };
--- ex10/solution.test.js
+++ ex10/solution.test.js
@@ -4,4 +4,8 @@ describe('testing the use case of the program', () => {
   it('checking if rhe alwaysThrows gives the right input', () => {
     expect(solution.alwaysThrows()).toEqual(Error('OH NOES'));
   });
+
+  it('checking if the iterate function is working', () => {
+    expect(solution.iterate(1)).toBe(2);
+  });
 });
