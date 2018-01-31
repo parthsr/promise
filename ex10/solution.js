42c5f96cbb7aae7a5873fff2b1f5176df4d51c3c
parth <srivastavaparth2016@gmail.com>
2018-01-31 12:45:07 +0530

    refractoring in the prpgram so that test case works

--- ex10/solution.js
+++ ex10/solution.js
@@ -1,9 +1,29 @@
-const alwaysThrows = () => new Error('OH NOES');
+const alwaysThrows = () => {
+  throw new Error('OH NOES');
+};
 const iterate = (arg) => {
   console.log(arg);
   return arg + 1;
 };
+
+let promise = Promise.resolve(iterate(1))
+  .then(iterate)
+  .then(iterate)
+  .then(iterate)
+  .then(iterate)
+  .then(alwaysThrows)
+  .then(iterate)
+  .then(iterate)
+  .then(iterate)
+  .then(iterate)
+  .then(iterate)
+  .catch((value) => {
+    console.log(value.message);
+    return value.message;
+  });
+
 module.exports = {
   alwaysThrows,
   iterate,
+  promise,
 };
--- ex10/solution.test.js
+++ ex10/solution.test.js
@@ -1,11 +1,9 @@
 const solution = require('./solution');
 
 describe('testing the use case of the program', () => {
-  it('checking if rhe alwaysThrows gives the right input', () => {
-    expect(solution.alwaysThrows()).toEqual(Error('OH NOES'));
-  });
+  it('checking if rhe alwaysThrows gives the right input', () => expect(solution.alwaysThrows).toThrow('OH NOES'));
 
-  it('checking if the iterate function is working', () => {
-    expect(solution.iterate(1)).toBe(2);
-  });
+  it('checking if the iterate function is working', () => expect(solution.iterate(1)).toBe(2));
+
+  it('checkind the chaining works or not', () => solution.promise.then(value => expect(value).toEqual('OH NOES')));
 });
