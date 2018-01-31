b7d58ba7d9785e84003d69d6a2c89735acac21fe
parth <srivastavaparth2016@gmail.com>
2018-01-31 10:24:19 +0530

    adding test case 2 for parsepromise

--- ex9/err.test.js
+++ ex9/err.test.js
@@ -4,4 +4,7 @@ describe('trying out the use case of the problem', () => {
   it('test case one to check what is printing with valid json', () => {
     parsePromised({ abc: 1 }).then(value => expect(value).toBe('parsing successful'));
   });
+  it('test case 2 to check what is printing with valid json', () => {
+    parsePromised(22).then(null, value => expect(value).toBe('parsing successful'));
+  });
 });
